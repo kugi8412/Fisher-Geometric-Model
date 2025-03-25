4c2bf65cb19cab10cd815920c9061f293bb4c18e
PiotrKupidura <piotrek.kupidura2@gmail.com>
2025-03-25 15:09:44 +0100

    Reproduction bugfix

--- reproduction.py
+++ reproduction.py
@@ -23,15 +23,30 @@ def reproduce(env):
     neighbors = torch.tensor(neighbors.squeeze(), device=device)
 
     phenotypes = pop.get_phenotypes()
-    radius = phenotypes[female_mask, 1] + 1.
+    radius = phenotypes[female_mask, 1] * 10
     reproduction_prob = 1 - pop.size / (2 * env.params['n_organisms'])
     
     is_reproducing = (distances < radius) & (torch.rand_like(distances) < reproduction_prob)
     
+    # Krzyżowanie genów
     if is_reproducing.any():
-        # Krzyżowanie genów
-        female_genotypes = pop.genotypes[female_mask][is_reproducing]
-        male_genotypes = pop.genotypes[male_mask][neighbors[is_reproducing]]
+        female_genotypes = pop.genotypes[female_mask]
+        females_pos = pop.positions[female_mask]
+        male_genotypes = pop.genotypes[male_mask][neighbors]
+        males_pos = pop.positions[male_mask][neighbors]
+        
+        # Dodanie wymiaru singletonowego usuniętego przy indeksowaniu
+        if len(female_genotypes.shape) == 2:
+            female_genotypes = female_genotypes.unsqueeze(0)
+            females_pos = females_pos.unsqueeze(0)
+        if len(male_genotypes.shape) == 2:
+            male_genotypes = male_genotypes.unsqueeze(0)
+            males_pos = males_pos.unsqueeze(0)
+        
+        female_genotypes = female_genotypes[is_reproducing]
+        females_pos = females_pos[is_reproducing]
+        male_genotypes = male_genotypes[is_reproducing]
+        males_pos = males_pos[is_reproducing]
         
         mask = torch.rand_like(female_genotypes[:, :-1]) < 0.5
         new_genotypes = torch.where(mask, female_genotypes[:, :-1], male_genotypes[:, :-1])
@@ -39,5 +54,5 @@ def reproduce(env):
         # Dodawanie nowych osobników
         new_sex = torch.randint(0, 2, (new_genotypes.shape[0], 1, 2), device=device)
         pop.genotypes = torch.cat([pop.genotypes, torch.cat([new_genotypes, new_sex], dim=1)])
-        pop.positions = torch.cat([pop.positions, (pop.positions[female_mask][is_reproducing] + pop.positions[male_mask][neighbors[is_reproducing]])/2])
+        pop.positions = torch.cat([pop.positions, (females_pos + males_pos)/2])
         pop.size = pop.genotypes.shape[0]
